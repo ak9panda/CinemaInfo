81d968e06e65d2359f4a7acbff7303de9151bfb0
akp <aungkyawphyoe@yomabank.com>
2021-02-15 11:16:13 +0630

    Initial Commit

---
+++ CinemaInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CinemaInfo
+//
+//  Created by admin on 15/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
